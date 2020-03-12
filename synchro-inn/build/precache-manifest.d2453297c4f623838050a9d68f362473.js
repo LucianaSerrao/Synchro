cc4a67b692b762124cdb65d10aa17ceeab50e4a6
LucianaSerrao <luliserrao@hotmail.com>
2020-03-12 14:15:42 -0300

    backup

---
+++ synchro-inn/build/precache-manifest.d2453297c4f623838050a9d68f362473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8611a43a4266a08035dd400e456e1cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87303491bc99345136bb",
+    "url": "/static/js/2.cdb5f153.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cdb5f153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e24dd3d59b13a1ca66",
+    "url": "/static/js/main.78ffe18f.chunk.js"
+  },
+  {
+    "revision": "469acadb5f8400854a64",
+    "url": "/static/js/runtime-main.4f7cc52a.js"
+  }
+]);
---
+++ synchro-inn/build/static/js/main.78ffe18f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsynchro-inn"]=this["webpackJsonpsynchro-inn"]||[]).push([[0],[,,,function(n,t,e){n.exports=e(4)},function(n,t,e){"use strict";e.r(t);e(0);var o=e(2),s=e.n(o),r=document.getElementById("root");s.a.render("ALOU",r)}],[[3,1,2]]]);
+//# sourceMappingURL=main.78ffe18f.chunk.js.map
